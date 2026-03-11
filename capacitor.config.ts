88fad986513b702c98260c2df5c275a61acdcacd
Yevhen <yevhen.o@2muchcoffee.com>
2026-03-11 16:26:19 +0200

    remove capacitor

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: '2mc.fusion.client',
-  appName: 'fusion-client',
-  webDir: 'dist',
-};
-
-export default config;
