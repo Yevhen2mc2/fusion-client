a2a7af2a1a8439cdaf096ee5437110b35cb4249c
Yevhen <yevhen.o@2muchcoffee.com>
2026-03-11 14:23:12 +0200

    add ionic and capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: '2mc.fusion.client',
+  appName: 'fusion-client',
+  webDir: 'dist',
+};
+
+export default config;
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { IonButton, IonDatetime, setupIonicReact } from '@ionic/react';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+setupIonicReact();
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <IonButton>Button</IonButton>
+      <IonDatetime></IonDatetime>
+    </>
+  );
 }
 
 export default App;
